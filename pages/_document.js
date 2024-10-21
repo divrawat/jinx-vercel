67b7142b07267980e86c8952313caf010aedd832
divrawat <divrawat2001@gmail>
2024-10-21 21:18:14 +0530

    sdf0se5f

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="6-XtSzr71_G64E3c4EdsVm7VnQEg2oKk7U9CASyxZKA" />
+      </Head>
       <body>
         <Main />
         <NextScript />
