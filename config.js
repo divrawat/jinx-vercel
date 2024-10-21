cd9505d126f79eafaad7b9a0a6eacaf20e516863
divrawat <divrawat2001@gmail>
2024-10-21 20:51:10 +0530

    srg0erg5

---
+++ config.js
@@ -0,0 +1,292 @@
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://jinxmanga.vercel.app';
+export const DOMAIN_NAME = 'jinxmanga.vercel.app';
+export const IMAGES_SUBDOMAIN = 'https://images.mangachimp.com';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Jinx</u></a> is a popular manhwa created by Mingwa. The story centers on Kim Dan, a physical therapist barely making ends meet, and Jaekyung Joo, a successful MMA fighter who believes his career is cursed by a "jinx" that holds him back. In a desperate attempt to break free from his bad luck, Jaekyung reaches out to Kim Dan for help. As the two are drawn together, their relationship becomes more intense and complicated, exploring deep themes of trauma, resilience, and the unconventional bond that grows between them.`
+
+
+export const MANGA_NAME = 'Jinx';
+export const MANGA_AUTHOR = 'Mingwa';
+export const MANGA_RELEASE = '2022';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Mingwa';
+export const MANGA_STUDIO = 'MAPPA';
+export const MANGA_GENRE = 'Romance, Yaoi, Drama, Sports';
+
+export const CHAPTER_PREFIX = 'jinx-chapter';
+export const IMAGE_PREFIX = 'images/jinx';
+export const NEXT_PREVIOUS_PREFIX = 'manga/jinx-chapter';
+export const URL_PREFIX = 'manga/jinx-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/jinx-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://cdn.epicstream.com/images/ncavvykf/epicstream/087c6bec73829312ec964ae04b7cfb0d8955fc2f-1255x738.png?rect=0,40,1255,659&w=1200&h=630&auto=format";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[135px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Jinx';
+export const NavbarName = "Jinx";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manhwa or manga all chapters online. The story centers on Kim Dan, a physical therapist barely making ends meet, and Jaekyung Joo, a successful MMA fighter who believes his career is cursed by a "jinx" that holds him back. In a desperate attempt to break free from his bad luck, Jaekyung reaches out to Kim Dan for help. As the two are drawn together, their relationship becomes more intense and complicated, exploring deep themes of trauma, resilience, and the unconventional bond that grows between them.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const navLinks = [
+    { text: 'Chapter 1', href: `${DOMAIN}/manga/jinx-chapter-10` },
+    { text: 'Chapter 54', href: `${DOMAIN}/manga/jinx-chapter-54` },
+    { text: 'Chapter 38', href: `${DOMAIN}/manga/jinx-chapter-38` },
+    // { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 96
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 61
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 50
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 67
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 61
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 66
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 54
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 55
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 49
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 61
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 57
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 63
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 63
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 63
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 71
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 70
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 7
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 71
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 60
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 48
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 69
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 82
+    },
+    {
+        "chapterNumber": "37a",
+        "numImages": 61
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "40a",
+        "numImages": 2
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 38
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 21
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "53a",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "53b",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "53c",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 39
+    }
+]
